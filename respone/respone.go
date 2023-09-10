c2576503476ae9e55f79fc90b967b6f575a68efd
lisonlin <lisonlin22@gmail.com>
2023-09-10 19:11:29 +0800

    增加 traceId

--- respone/respone.go
+++ respone/respone.go
@@ -7,13 +7,15 @@ import (
 )
 
 type Body struct {
-	Code int         `json:"code"`
-	Msg  string      `json:"msg"`
-	Data interface{} `json:"data,omitempty"`
+	Code    int         `json:"code"`
+	Msg     string      `json:"msg"`
+	Data    interface{} `json:"data,omitempty"`
+	TraceId string      `json:"trace_id"`
 }
 
-func Response(w http.ResponseWriter, resp interface{}, err error) {
+func Response(w http.ResponseWriter, resp interface{}, traceId string, err error) {
 	var body Body
+	body.TraceId = traceId
 	if err != nil {
 		body.Code = -1
 		body.Msg = err.Error()
